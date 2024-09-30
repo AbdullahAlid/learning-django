29f3bcfb61abb5916f0b6c9c7c504e991c3971f8
Abdullah Alid <alidabdullah404@gmail.com>
2024-09-30 12:10:26 +0600

    intro

---
+++ myproject/myproject/views.py
@@ -0,0 +1,12 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    # return HttpResponse('this is home page')
+    return render(request, 'home.html')
+
+
+def about(request):
+    # return HttpResponse('this is about page')
+    return render(request, 'about.html')
